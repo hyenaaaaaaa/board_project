e7edafea7279c63e00a891d5bdeeaffd49085872
urbanecho97@gmail.com <urbanecho97@gmail.com>
2023-11-13 15:11:19 +0900

    모바일, PC 분리

--- src/main/java/org/koreait/project/commons/Utils.java
+++ src/main/java/org/koreait/project/commons/Utils.java
@@ -1,14 +1,20 @@
 package org.koreait.project.commons;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.Objects;
 import java.util.ResourceBundle;
 
 @Component
+@RequiredArgsConstructor
 public class Utils {
     private static ResourceBundle validationsBundle;
     private static ResourceBundle errorsBundle;
+    private final HttpServletRequest request;
+    private final HttpSession session;
 
     static {
         validationsBundle = ResourceBundle.getBundle("messages.validation");
@@ -24,5 +30,23 @@ public class Utils {
         } catch (Exception e) {
             return null;
         }
+    }
+
+    public boolean isMobile() { // 접속한 것이 모바일인지 pc인지 판별
+
+        String device = (String) session.getAttribute("device");
+        if (device != null) {
+            return device.equals("mobile");
+        }
+
+        //요청 헤더 User-Agent
+        boolean isMobile = request.getHeader("User-Agent").matches(".*(iPhone|iPod|iPad|BlackBerry|Android|Windows CE|LG|MOT|SAMSUNG|SonyEricsson).*");
+
+
+        return isMobile;
+    }
+
+    public String tpl(String tplPath) { // 접속한 것이 모바일인지 pc인지 판별 후 템플릿 결정
+        return String.format("%s/" + tplPath, isMobile() ? "mobile" : "front");
     }
 }
---
+++ src/main/java/org/koreait/project/controllers/members/MemberController.java
@@ -0,0 +1,21 @@
+package org.koreait.project.controllers.members;
+
+
+import lombok.RequiredArgsConstructor;
+import org.koreait.project.commons.Utils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+@RequiredArgsConstructor
+public class MemberController {
+    private final Utils utils;
+
+    @GetMapping("/join")
+    public String join() {
+
+        return utils.tpl("member/join");
+    }
+}
