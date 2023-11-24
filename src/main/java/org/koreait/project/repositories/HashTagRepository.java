43ff1d955b40ca7f2dff9266037abc603a34a1ea
urbanecho97@gmail.com <urbanecho97@gmail.com>
2023-11-24 17:54:57 +0900

    수정

--- src/main/java/org/koreait/project/repositories/HashTagRepository.java
+++
@@ -1,8 +0,0 @@
-package org.koreait.project.repositories;
-
-import org.koreait.project.entities.HashTag;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HashTagRepository extends JpaRepository<HashTag, String> {
-
-}
--- src/main/java/org/koreait/project/repositories/MemberProfileRepository.java
+++
@@ -1,8 +0,0 @@
-package org.koreait.project.repositories;
-
-import org.koreait.project.entities.MemberProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberProfileRepository extends JpaRepository<MemberProfile, Long> {
-
-}
